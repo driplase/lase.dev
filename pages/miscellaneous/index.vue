20faae2412894c8624b0b8968ac7921fb33119e1
Driplase <258vr.g.subspeed@gmail.com>
2025-08-15 15:00:02 +0900

    chore: set origin-left

--- pages/miscellaneous/index.vue
+++ pages/miscellaneous/index.vue
@@ -3,8 +3,9 @@
 <template>
   <NuxtLayout name="default" :title="$t('miscellaneous')" align="start">
     <div class="container">
-      <h1 class="flash woosh">{{ $t('miscellaneous') }}</h1>
-      <h2 class="woosh">Discord</h2>
+      <h1 class="flash woosh origin-left">{{ $t('miscellaneous') }}</h1>
+
+      <h2 class="woosh origin-left">Discord</h2>
       <WorksCards :title="$t('misc.rcp.title')" url="/miscellaneous/discord/role-color-preview">
         {{ $t('misc.list.rcp.description') }}
       </WorksCards>
