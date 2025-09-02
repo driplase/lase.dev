e1ee7f7d88aa4ff85abec9cefc021977fcf4c692
Driplase <258vr.g.subspeed@gmail.com>
2025-09-02 23:22:32 +0900

    feat: use json to easily change link list - move rampotchi's website to reciprocal, changed hostname

---
+++ data/links.js
@@ -0,0 +1,126 @@
+export default {
+  reciprocal: [
+    {
+      url: "https://furamen.kokkosoft.com",
+      title: "ふらめんのホームページ",
+      description: "",
+      imageURL: "https://7xf.j0.si/banners/furamen.webp",
+    },
+    {
+      url: "https://spacer4719.f5.si",
+      title: "spacer4719's site",
+      description: "",
+      imageURL: "https://spacer4719.f5.si/images/NSH.webp",
+    },
+    {
+      url: "https://kntnkmr.f5.si",
+      title: "Kntnkmr",
+      description: "",
+    },
+    {
+      url: "https://9a.si/",
+      title: "9a.si",
+      description: "",
+      imageURL: "https://9a.si/9a.si.webp",
+    },
+    {
+      url: "https://hamusata.f5.si",
+      title: "HAMUSATA homepage",
+      description: "",
+      imageURL: "https://hamusata.f5.si/banner_icon_hamusata.webp",
+    },
+    {
+      url: "https://rampotchi.com",
+      title: "ランポっちのホームページ",
+      description: "",
+      imageURL: "https://rampotchi.com/img/logo.webp",
+    },
+  ],
+  others: [
+    {
+      url: "https://github.com/driplase/lase.dev",
+      title: "driplase/lase.dev (Source Code)",
+      description: "",
+    },
+    {
+      url: "https://vuejs.org/",
+      title: "Vue.js - The Progressive JavaScript Framework",
+      description: "",
+    },
+    {
+      url: "https://nuxt.com",
+      title: "Nuxt - The Progressive Web Framework",
+      description: "",
+    },
+    {
+      url: "https://gsap.com",
+      title: "GSAP - A wildly robust JavaScript animation library",
+      description: "",
+    },
+    {
+      url: "https://j0.si/",
+      title: "j0.si",
+      description: "",
+    },
+    {
+      url: "https://laselxt.vercel.app",
+      title: "Laselxt",
+      description: "",
+    },
+    {
+      url: "https://0800300.xyz",
+      title: "0800300.xyz",
+      description: "",
+    },
+    {
+      url: "https://ljsa.win",
+      title: "ljsa.win",
+      description: "",
+    },
+    {
+      url: "https://3x.f5.si",
+      title: "3x.f5.si",
+      description: "",
+    },
+    {
+      url: "https://driplase.github.io",
+      title: "driplase.github.io",
+      description: "",
+    },
+    {
+      url: "https://jli.pages.dev",
+      title: "jli.pages.dev",
+      description: "",
+    },
+    {
+      url: "https://scratchjp.notion.site/Scratch-JP-4cf67387576f4fccb9c8a3eb8ca53a0a",
+      title: "Scratch JP (Notion)",
+      description: "",
+    },
+    {
+      url: "https://mcscr.jp",
+      title: "ScJPMCマイクラ鯖",
+      description: "",
+    },
+    {
+      url: "https://kouhei.f5.si",
+      title: "Kouheiのホームページ",
+      description: "",
+    },
+    {
+      url: "https://kokkosoft.com/",
+      title: "KokkoSoft OfficialPage",
+      description: "",
+    },
+    {
+      url: "https://iseebi.kokkosoft.com/",
+      title: "いせえびのホームページ",
+      description: "",
+    },
+    {
+      url: "https://treron.jp/",
+      title: "Treron.jp",
+      description: "",
+    },
+  ],
+}
--- pages/links.vue
+++ pages/links.vue
@@ -1,4 +1,6 @@
 <script setup>
+import linksData from '@/data/links'
+
 useSeoMeta({
   robots: "noindex, nofollow"
 })
@@ -66,71 +68,27 @@ useSeoMeta({
     
     <h2 class="flash woosh font-bold origin-left" id="reciprocal">{{ $t('mutual_links') }}</h2>
     <div class="socials">
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink class="" href="https://furamen.kokkosoft.com" target="_blank" rel="noopener noreferrer">
+      <p 
+        v-for="link in linksData.reciprocal.sort((a, b) => a.title.localeCompare(b.title))"
+        class="wiggle woosh wg-text wsh-text flash links"
+      >
+        <NuxtLink class="" :href="link.url" target="_blank" rel="noopener noreferrer">
           <!-- <i class="eclyera icon">&#xf806;</i> -->
           <NuxtImg
+            v-if="link.imageURL"
             class="link-header"
-            src="https://7xf.j0.si/banners/furamen.webp" 
-            alt="ふらめんのホームページ"
+            :src="link.imageURL" 
+            :alt="link.title"
             decoding="async"
             draggable="false"
-            fetchpriority="high"
           />
-        </NuxtLink>
-      </p>
-      
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink href="https://spacer4719.f5.si" target="_blank" rel="noopener noreferrer">
-          <NuxtImg
-            class="link-header"
-            src="https://spacer4719.f5.si/images/NSH.webp" 
-            alt="spacer4719's site"
-            decoding="async"
-            draggable="false"
-            fetchpriority="high"
-          />
-        </NuxtLink>
-      </p>
-      
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink href="https://kntnkmr.f5.si" target="_blank" rel="noopener noreferrer">
-          Kntnkmr
-          <Icon
-          name="tabler:external-link" 
-          class="icon"
-          />
-        </NuxtLink>
-      </p>
-      
-      <p class="wiggle woosh wg-text wsh-text flash links zen-maru-gothic">
-        <NuxtLink href="https://9a.si/" target="_blank" rel="noopener noreferrer">
-          <!-- 
-            9a.si
+          <span v-else>
+            {{ link.title }}
             <Icon
               name="tabler:external-link" 
               class="icon"
             />
-          -->
-          <NuxtImg
-            class="link-header"
-            src="https://9a.si/9a.si.webp"
-            alt="9a.si"
-            decoding="async"
-            draggable="false"
-          />
-        </NuxtLink>
-      </p>
-
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink href="https://hamusata.f5.si" target="_blank" rel="noopener noreferrer">
-          <NuxtImg
-            class="link-header"
-            src="https://hamusata.f5.si/banner_icon_hamusata.webp"
-            alt="HAMUSATA homepage"
-            decoding="async"
-            draggable="false"
-          />
+          </span>
         </NuxtLink>
       </p>
 
@@ -141,200 +99,27 @@ useSeoMeta({
     <div class="socials">
       <small class="opacity-72 whitespace-pre text-xs mb-1">{{ $t('links.others.desc') }}</small>
       
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink href="https://github.com/driplase/lase.dev" target="_blank" rel="noopener noreferrer">
-          driplase/lase.dev (Source Code)
-          <Icon
-            name="tabler:external-link" 
-            class="icon"
-          />
-        </NuxtLink>
-      </p>
-      
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink href="https://vuejs.org/" target="_blank" rel="noopener noreferrer">
-          <Icon
-          name="logos:vue" 
-          class="icon"
-          />
-          Vue.js - The Progressive JavaScript Framework
-          <Icon
-            name="tabler:external-link" 
-            class="icon"
-            />
-          </NuxtLink>
-        </p>
-        
-        <p class="wiggle woosh wg-text wsh-text flash links">
-          <NuxtLink href="https://nuxt.com" target="_blank" rel="noopener noreferrer">
-            <Icon
-            name="logos:nuxt-icon" 
-            class="icon"
-            />
-          Nuxt - The Progressive Web Framework
-          <Icon
-          name="tabler:external-link" 
-            class="icon"
-          />
-        </NuxtLink>
-      </p>
-      
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink href="https://gsap.com" target="_blank" rel="noopener noreferrer">
-          GSAP - A wildly robust JavaScript animation library
-          <Icon
-            name="tabler:external-link" 
-            class="icon"
-          />
-        </NuxtLink>
-      </p>
-      
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink href="https://j0.si/" target="_blank" rel="noopener noreferrer">
-          j0.si
-          <Icon
-            name="tabler:external-link" 
-            class="icon"
-          />
-        </NuxtLink>
-      </p>
-      
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink href="https://laselxt.vercel.app" target="_blank" rel="noopener noreferrer">
-          Laselxt
-          <Icon
-            name="tabler:external-link" 
-            class="icon"
-            />
-          </NuxtLink>
-      </p>
-      
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink href="https://0800300.xyz" target="_blank" rel="noopener noreferrer">
-          0800300.xyz
-          <Icon
-            name="tabler:external-link" 
-            class="icon"
-            />
-          </NuxtLink>
-      </p>
-      
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink href="https://ljsa.win" target="_blank" rel="noopener noreferrer">
-          ljsa.win
-          <Icon
-            name="tabler:external-link" 
-            class="icon"
-            />
-          </NuxtLink>
-      </p>
-      
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink href="https://3x.f5.si" target="_blank" rel="noopener noreferrer">
-          3x.f5.si
-          <Icon
-            name="tabler:external-link" 
-            class="icon"
-            />
-          </NuxtLink>
-      </p>
-      
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink href="https://driplase.github.io" target="_blank" rel="noopener noreferrer">
-          driplase.github.io
-          <Icon
-            name="tabler:external-link" 
-            class="icon"
-            />
-          </NuxtLink>
-      </p>
-      
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink href="https://jli.pages.dev" target="_blank" rel="noopener noreferrer">
-          jli.pages.dev
-          <Icon
-            name="tabler:external-link" 
-            class="icon"
-            />
-          </NuxtLink>
-      </p>
-      
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink href="https://scratchjp.notion.site/Scratch-JP-4cf67387576f4fccb9c8a3eb8ca53a0a" target="_blank" rel="noopener noreferrer">
-          Scratch JP (Notion)
-          <Icon
-            name="tabler:external-link" 
-            class="icon"
-          />
-        </NuxtLink>
-      </p>
-      
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink href="https://mcscr.jp" target="_blank" rel="noopener noreferrer">
+      <p 
+        v-for="link in linksData.others"
+        class="wiggle woosh wg-text wsh-text flash links"
+      >
+        <NuxtLink class="" :href="link.url" target="_blank" rel="noopener noreferrer">
+          <!-- <i class="eclyera icon">&#xf806;</i> -->
           <NuxtImg
-            class="icon"
-            src="https://map.mcscr.jp/logo/64x64.webp"
-            alt="ScJPMC"
-            decoding="async"
-          />
-          ScJPMCマイクラ鯖
-          <Icon
-            name="tabler:external-link" 
-            class="icon"
-          />
-        </NuxtLink>
-      </p>
-      
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink href="https://rampo.f5.si" target="_blank" rel="noopener noreferrer">
-          <NuxtImg
+            v-if="link.imageURL"
             class="link-header"
-            src="https://rampo.f5.si/img/logo.webp"
-            alt="ランポっちのホームページ"
+            :src="link.imageURL" 
+            :alt="link.title"
             decoding="async"
             draggable="false"
           />
-        </NuxtLink>
-      </p>
-      
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink href="https://kouhei.f5.si" target="_blank" rel="noopener noreferrer">
-          Kouheiのホームページ
-          <Icon
-            name="tabler:external-link" 
-            class="icon"
-          />
-        </NuxtLink>
-      </p>
-
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink href="https://kokkosoft.com/" target="_blank" rel="noopener noreferrer">
-          <i class="eclyera icon">&#xf807;</i>
-          KokkoSoft OfficialPage
-          <Icon
-            name="tabler:external-link" 
-            class="icon"
-          />
-        </NuxtLink>
-      </p>
-
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink href="https://iseebi.kokkosoft.com/" target="_blank" rel="noopener noreferrer">
-          いせえびのホームページ
+          <span v-else>
+            {{ link.title }}
             <Icon
               name="tabler:external-link" 
               class="icon"
             />
-        </NuxtLink>
-      </p>
-
-      <p class="wiggle woosh wg-text wsh-text flash links">
-        <NuxtLink href="https://treron.jp/" target="_blank" rel="noopener noreferrer">
-          Treron.jp
-          <Icon
-            name="tabler:external-link" 
-            class="icon"
-          />
+          </span>
         </NuxtLink>
       </p>
     </div>
