1d80e19d27c22268876edaf30b6e3060ceaf48dc
Driplase <258vr.g.subspeed@gmail.com>
2025-11-24 09:19:14 +0900

    fix: bookmarklet doesn't work

--- pages/chores/cxkes-xuid-fg.vue
+++ pages/chores/cxkes-xuid-fg.vue
@@ -1,3 +1,7 @@
+<script setup>
+const bookmarklet = "javascript:(()=>{if(location.hostname + location.pathname !== \x22cxkes.me/xbox/xuid\x22){window.open('https://cxkes.me/xbox/xuid', \x22_blank\x22);return;}const xuidHex = document.getElementById('xuidHex').textContent; document.querySelector('div.col-md-12 > hr:nth-of-type(2)').insertAdjacentHTML('beforebegin',`<br><strong>Floodgate UUID</strong>: <code id=\x22uuidFloodgate\x22>00000000-0000-0000-${xuidHex.slice(0,4)}-${xuidHex.slice(4)}</code> <i class=\x22bi bi-clipboard\x22 data-toggle=\x22tooltip\x22 data-placement=\x22top\x22 title=\x22Copy\x22 onclick=\x22copyToClipboard('uuidFloodgate')\x22></i><br>`)})()";
+</script>
+
 <template>
   <div class="w-full m-4">
     <p>
@@ -6,9 +10,7 @@
 
     <p>
       drag this to the favorite bar ==> 
-      <NuxtLink to="javascript:(()=>{if(location.hostname + location.pathname !== \x22cxkes.me/xbox/xuid\x22){window.open('https://cxkes.me/xbox/xuid', \x22_blank\x22);return;}const xuidHex = document.getElementById('xuidHex').textContent; document.querySelector('div.col-md-12 > hr:nth-of-type(2)').insertAdjacentHTML('beforebegin',`<br><strong>Floodgate UUID</strong>: <code id=\x22uuidFloodgate\x22>00000000-0000-0000-${xuidHex.slice(0,4)}-${xuidHex.slice(4)}</code> <i class=\x22bi bi-clipboard\x22 data-toggle=\x22tooltip\x22 data-placement=\x22top\x22 title=\x22Copy\x22 onclick=\x22copyToClipboard('uuidFloodgate')\x22></i><br>`)})()">
-        Floodgate UUID Lookup
-      </NuxtLink>
+      <NuxtLink :to="bookmarklet">Floodgate UUID Lookup</NuxtLink>
     </p>
 
     <hr class="glow-line shadow-gray-300 my-4" />
@@ -20,9 +22,7 @@
 
     <p>
       お気に入りバーにこいつをぶちこんで →→→ 
-      <NuxtLink to="javascript:(()=>{if(location.hostname + location.pathname !== \x22cxkes.me/xbox/xuid\x22){window.open('https://cxkes.me/xbox/xuid', \x22_blank\x22);return;}const xuidHex = document.getElementById('xuidHex').textContent; document.querySelector('div.col-md-12 > hr:nth-of-type(2)').insertAdjacentHTML('beforebegin',`<br><strong>Floodgate UUID</strong>: <code id=\x22uuidFloodgate\x22>00000000-0000-0000-${xuidHex.slice(0,4)}-${xuidHex.slice(4)}</code> <i class=\x22bi bi-clipboard\x22 data-toggle=\x22tooltip\x22 data-placement=\x22top\x22 title=\x22Copy\x22 onclick=\x22copyToClipboard('uuidFloodgate')\x22></i><br>`)})()">
-        Floodgate UUID Lookup
-      </NuxtLink>
+      <NuxtLink :to="bookmarklet">Floodgate UUID Lookup</NuxtLink>
     </p>
   </div>
 </template>
