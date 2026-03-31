26af8273ecff9c1cea3a7bb7d10d79a4d9ea154d
driplase <d@j0.si>
2026-03-31 19:23:03 +0900

    chore: didn't know i needed to use { external: true } for redirecting external links

--- pages/chores/avtives.vue
+++ pages/chores/avtives.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 const targetURL = "https://driplase.booth.pm/item_lists/mvdTgZeD";
 
-navigateTo(targetURL);
+navigateTo(targetURL, { external: true });
 </script>
+<template>
+  <p>should be redirected to the Avtives list of my BOOTH shop...</p>
+</template>
